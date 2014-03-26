b4e565bf91c124b6565f1282fd24ac7dfc26dbce
orangeejs <orangeejs@orangeejs.com>
2014-03-26 00:06:34 -0400

    add default icon

--- bin/tasks/build.js
+++ bin/tasks/build.js
@@ -1,11 +1,27 @@
+var fs = require('fs');
+
 function OrangeeJSBuildTask() {
 };
 
-OrangeeJSBuildTask.prototype.run = function() {
-  mkdir('-p', 'build/samsung');
+OrangeeJSBuildTask.prototype.run = function(name) {
+  this._build_samsung();
   mkdir('-p', 'build/lg');
   mkdir('-p', 'build/vizio')
   mkdir('-p', 'build/html5');
 };
+
+OrangeeJSBuildTask.prototype._build_samsung = function() {
+  console.log("build samsung");
+  mkdir('-p', 'build/samsung');
+  var content = "Use Alpha Blending? = Yes\n" + 
+    "Screen Resolution = 1280x720";
+  fs.writeFile("build/samsung/widget.info", content, function(err) {
+    if(err) {
+      console.log(err);
+    } else {
+      console.log("The file was saved!");
+    }
+  }); 
+}
 
 module.exports = OrangeeJSBuildTask;
--- bin/tasks/init.js
+++ bin/tasks/init.js
@@ -1,8 +1,22 @@
+var fs = require('fs');
+
 function OrangeeJSInitTask() {
 };
 
 OrangeeJSInitTask.prototype.run = function() {
-  mkdir('-p', 'app');
+  fs.exists('app', function(exists) {
+    if (!exists) {
+      mkdir('-p', 'app');
+      console.log("created app folder")
+    }
+  });
+
+  fs.exists('app/icons', function(exists) {
+    if (!exists) {
+      mkdir('-p', 'app/icons');
+      cp('src/icons/*.png', 'app/icons')
+    }
+  });
 };
 
 module.exports = OrangeeJSInitTask;
