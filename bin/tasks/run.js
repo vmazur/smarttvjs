d00b66948b1cbc83d993a27caebc586ba4c6eebb
orangeejs <orangeejs@orangeejs.com>
2014-03-28 00:15:30 -0400

    fix icon path, auto mount Apps

--- bin/tasks/run.js
+++ bin/tasks/run.js
@@ -11,7 +11,7 @@ OrangeeJSRunTask.prototype.run = function(name) {
   if (name === 'samsung') {
     exec('VBoxManage controlvm "2014_Smart_TV_Emulator_5_0" poweroff');
     exec('VBoxManage sharedfolder remove "2014_Smart_TV_Emulator_5_0" -name "Apps"');
-    exec('VBoxManage sharedfolder add "2014_Smart_TV_Emulator_5_0" -name "Apps" -hostpath "' + process.cwd() + '/build"');
+    exec('VBoxManage sharedfolder add "2014_Smart_TV_Emulator_5_0" -name "Apps" -hostpath "' + process.cwd() + '/build" --automount');
     exec('VBoxManage startvm 2014_Smart_TV_Emulator_5_0');
   }
 };
