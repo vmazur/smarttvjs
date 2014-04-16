1c8158bc37a00598f272340175ee8a7858eebb1c
Raecoo Cao <raecoo@gmail.com>
2014-04-16 13:03:15 +0800

    adds Snap.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,9 @@
 module.exports = function(grunt) {
 
+  var files = ['src/widgets/modal.js', 'src/widgets/button.js',
+    'src/widgets/popover.js', 'src/widgets/tab.js',
+    'src/vendor/snap.min.js'
+    ]
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
@@ -7,9 +11,12 @@ module.exports = function(grunt) {
       options: {
         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
       },
-      build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
+      // build: {
+      //   src: 'src/<%= pkg.name %>.js',
+      //   dest: 'build/<%= pkg.name %>.min.js'
+      // },
+      release: {
+        files: {'dest/<%= pkg.name %>.min.js': files }
       }
     }
   });
