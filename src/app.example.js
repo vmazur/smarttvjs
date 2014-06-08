20fe5a7a137347d7ee2a8f62ff296fa072e90c51
orangeejs <orangeejs@orangeejs.com>
2014-06-08 18:05:56 -0400

    fix onplaying event

--- src/app.example.js
+++ src/app.example.js
@@ -11,7 +11,13 @@ app.init = function() {
     {url: "https://www.youtube.com/watch?v=2Zj_kxYBu1Y", name: "youtube video"},
     {url: "http://techslides.com/demos/sample-videos/small.mp4", name: "mp4 video"}
   ];
-  app.videoplayer.load(playlist, 0, 'player_target', {width: 560, height: 315})
+  app.videoplayer.load(playlist, 0, 'player_target', {
+    width: 560,
+    height: 315,
+    onplaying: function() {
+      console.log('playing');
+    }
+  });
 
   orangee.render_template("#videos_target", "#videos_template", {data: playlist});
 };
--- src/ytplayer.js
+++ src/ytplayer.js
@@ -1,7 +1,6 @@
 //it is better to wait until onYouTubePlayerAPIReady(playerId)
 orangee.ytplayer = function() {
   this.player = null;
-  this.options = {};
 };
 
 orangee.ytplayer.prototype.play = function() {
@@ -22,7 +21,6 @@ orangee.ytplayer.prototype.currentTime = function() {
 
 orangee.ytplayer.prototype.load = function(url, lastPosition, divid, options) {
   var vid = url.split('watch?v=')[1];
-  this.options = options;
 
   if (this.player) {
     this.player.loadVideoById(vid);
@@ -46,60 +44,17 @@ orangee.ytplayer.prototype.load = function(url, lastPosition, divid, options) {
         'hd': 1
       },
       events: {
-        'onReady': this.onPlayerReady,
-        'onStateChange': this.onPlayerStateChange,
-        'onError': this.onPlayerError
+        /*'onReady': function() {
+        },
+        'onError': function() {
+        },*/
+        'onStateChange': function(event) {
+          if (event.data == YT.PlayerState.PLAYING && options['onplaying']) {
+            options['onplaying']();
+          }
+        }
       }
     });
   }
 };
-
-// This function is called when the player changes state
-orangee.ytplayer.prototype.onPlayerStateChange = function(event) {
-  /*if(event.data == 0) {
-    //document.getElementById('logs').innerHTML += currentVideo
-    this.Next();
-  } else if (event.data == 1 && this.lastPosition !=0) {
-    this.player.seekTo(this.lastPosition, true);
-    this.lastPosition = 0;
-  }*/
-  // Can not use this in callback
-  if (event.data == YT.PlayerState.PLAYING && this.options['onplaying']) {
-    this.options['onplaying']();
-  }
-};
-
-orangee.ytplayer.prototype.onPlayerReady = function(event) {
-  //player.setPlaybackQuality("hd720");
-  //player.setVolume(100);
-  //player.playVideo(); calling this will cause problem on iphone
-  if (this.options && this.options['onready']) {
-    this.options['onready']();
-  }
-};
-
-orangee.ytplayer.prototype.onPlayerError = function(event) {
-  if ((event.data == 2 ||
-      event.data == 100 ||
-      event.data == 101 ||
-      event.data == 150) && this.options['onerror']) {
-    this.options['onerror']();
-  }
-};
-
-/*
-loadProgress: function(url) {
-  var vid = url.split('watch?v=')[1];
-  var v = readCookie("youtube_progress_" + "TODO!!!!!");//FIXME
-  if (v != null && v != "") {
-    a = v.split(":");
-    currentVideo = parseInt(a[0]);
-    if (this.currentVideo >= this.playlist.length) {
-      this.currentVideo = 0; //playlist changed, cookie is invalid
-    } else if (a.length > 1) {
-      this.lastPosition = parseInt(a[1]);
-    }
-  }
-
-},*/
 
