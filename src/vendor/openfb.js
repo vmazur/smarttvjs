4a67c88508318313b1d6e84391255402279ebb39
orangeejs <orangeejs@orangeejs.com>
2014-08-24 16:34:02 -0400

    update openfb.js

--- src/vendor/openfb.js
+++ src/vendor/openfb.js
@@ -5,28 +5,38 @@
  * Cordova, you also don't need the Facebook Cordova plugin. There is also no dependency on jQuery.
  * OpenFB allows you to login to Facebook and execute any Facebook Graph API request.
  * @author Christophe Coenraets @ccoenraets
- * @version 0.3
+ * @version 0.4
  */
-orangee.facebook = (function () {
+var openFB = (function () {
 
     var FB_LOGIN_URL = 'https://www.facebook.com/dialog/oauth',
+        FB_LOGOUT_URL = 'https://www.facebook.com/logout.php',
 
-    // By default we store fbtoken in sessionStorage. This can be overridden in init()
+        // By default we store fbtoken in sessionStorage. This can be overridden in init()
         tokenStore = window.sessionStorage,
 
         fbAppId,
-        oauthRedirectURL,
 
-    // Because the OAuth login spans multiple processes, we need to keep the success/error handlers as variables
-    // inside the module instead of keeping them local within the login function.
-        loginSuccessHandler,
-        loginErrorHandler,
+        context = window.location.pathname.substring(0, window.location.pathname.indexOf("/",2)),
 
-    // Indicates if the app is running inside Cordova
+        baseURL = location.protocol + '//' + location.hostname + (location.port ? ':' + location.port : '') + context,
+
+        oauthRedirectURL = baseURL + '/oauthcallback.html',
+
+        logoutRedirectURL = baseURL + '/logoutcallback.html',
+
+        // Because the OAuth login spans multiple processes, we need to keep the login callback function as a variable
+        // inside the module instead of keeping it local within the login function.
+        loginCallback,
+
+        // Indicates if the app is running inside Cordova
         runningInCordova,
 
-    // Used in the exit event handler to identify if the login has already been processed elsewhere (in the oauthCallback function)
+        // Used in the exit event handler to identify if the login has already been processed elsewhere (in the oauthCallback function)
         loginProcessed;
+
+    console.log(oauthRedirectURL);
+    console.log(logoutRedirectURL);
 
     document.addEventListener("deviceready", function () {
         runningInCordova = true;
@@ -35,31 +45,59 @@ orangee.facebook = (function () {
     /**
      * Initialize the OpenFB module. You must use this function and initialize the module with an appId before you can
      * use any other function.
-     * @param appId - The id of the Facebook app
-     * @param redirectURL - The OAuth redirect URL. Optional. If not provided, we use sensible defaults.
-     * @param store - The store used to save the Facebook token. Optional. If not provided, we use sessionStorage.
+     * @param params - init paramters
+     *  appId: The id of the Facebook app,
+     *  tokenStore: The store used to save the Facebook token. Optional. If not provided, we use sessionStorage.
      */
-    function init(appId, redirectURL, store) {
-        fbAppId = appId;
-        if (redirectURL) oauthRedirectURL = redirectURL;
-        if (store) tokenStore = store;
+    function init(params) {
+        if (params.appId) {
+            fbAppId = params.appId;
+        } else {
+            throw 'appId parameter not set in init()';
+        }
+
+        if (params.tokenStore) {
+            tokenStore = params.tokenStore;
+        }
+    }
+
+    /**
+     * Checks if the user has logged in with openFB and currently has a session api token.
+     * @param callback the function that receives the loginstatus
+     */
+    function getLoginStatus(callback) {
+        var token = tokenStore['fbtoken'],
+            loginStatus = {};
+        if (token) {
+            loginStatus.status = 'connected';
+            loginStatus.authResponse = {token: token};
+        } else {
+            loginStatus.status = 'unknown';
+        }
+        if (callback) callback(loginStatus);
     }
 
     /**
      * Login to Facebook using OAuth. If running in a Browser, the OAuth workflow happens in a a popup window.
      * If running in Cordova container, it happens using the In-App Browser. Don't forget to install the In-App Browser
      * plugin in your Cordova project: cordova plugins add org.apache.cordova.inappbrowser.
-     * @param scope - The set of Facebook permissions requested
-     * @param success - Callback function to invoke when the login process succeeds
-     * @param error - Callback function to invoke when the login process fails
+     *
+     * @param callback - Callback function to invoke when the login process succeeds
+     * @param options - options.scope: The set of Facebook permissions requested
      * @returns {*}
      */
-    function login(scope, success, error) {
+    function login(callback, options) {
 
         var loginWindow,
-            startTime;
+            startTime,
+            scope = '';
 
-        function loginWindowLoadStart(event) {
+        if (!fbAppId) {
+            return callback({status: 'unknown', error: 'Facebook App Id not set.'});
+        }
+
+        // Inappbrowser load start handler: Used when running in Cordova only
+        function loginWindow_loadStartHandler(event) {
             var url = event.url;
             if (url.indexOf("access_token=") > 0 || url.indexOf("error=") > 0) {
                 // When we get the access token fast, the login window (inappbrowser) is still opening with animation
@@ -72,48 +110,38 @@ orangee.facebook = (function () {
             }
         }
 
-        function loginWindowExit() {
+        // Inappbrowser exit handler: Used when running in Cordova only
+        function loginWindow_exitHandler() {
             console.log('exit and remove listeners');
             // Handle the situation where the user closes the login window manually before completing the login process
             deferredLogin.reject({error: 'user_cancelled', error_description: 'User cancelled login process', error_reason: "user_cancelled"});
-            loginWindow.removeEventListener('loadstop', loginWindowLoadStart);
-            loginWindow.removeEventListener('exit', loginWindowExit);
+            loginWindow.removeEventListener('loadstop', loginWindow_loadStartHandler);
+            loginWindow.removeEventListener('exit', loginWindow_exitHandler);
             loginWindow = null;
             console.log('done removing listeners');
         }
 
-
-        if (!fbAppId) {
-            return error({error: 'Facebook App Id not set.'});
+        if (options && options.scope) {
+            scope = options.scope;
         }
 
-        scope = scope || '';
-
-        loginSuccessHandler = success;
-        loginErrorHandler = error;
-
+        loginCallback = callback;
         loginProcessed = false;
-        logout();
 
-        // Check if an explicit oauthRedirectURL has been provided in init(). If not, infer the appropriate value
-        if (!oauthRedirectURL) {
-            if (runningInCordova) {
-                oauthRedirectURL = 'https://www.facebook.com/connect/login_success.html';
-            } else {
-                var origin = location.protocol + '//' + location.hostname + (location.port ? ':' + location.port : '');
-                // could also use 'var origin = window.location.origin' when enough browsers support it
-                oauthRedirectURL = origin + '/oauthcallback.html';
-            }
+//        logout();
+
+        if (runningInCordova) {
+            oauthRedirectURL = "https://www.facebook.com/connect/login_success.html";
         }
 
         startTime = new Date().getTime();
         loginWindow = window.open(FB_LOGIN_URL + '?client_id=' + fbAppId + '&redirect_uri=' + oauthRedirectURL +
-            '&response_type=token&display=popup&scope=' + scope, '_blank', 'location=no');
+            '&response_type=token&scope=' + scope, '_blank', 'location=no');
 
         // If the app is running in Cordova, listen to URL changes in the InAppBrowser until we get a URL with an access_token or an error
         if (runningInCordova) {
-            loginWindow.addEventListener('loadstart', loginWindowLoadStart);
-            loginWindow.addEventListener('exit', loginWindowExit);
+            loginWindow.addEventListener('loadstart', loginWindow_loadStartHandler);
+            loginWindow.addEventListener('exit', loginWindow_exitHandler);
         }
         // Note: if the app is running in the browser the loginWindow dialog will call back by invoking the
         // oauthCallback() function. See oauthcallback.html for details.
@@ -136,21 +164,41 @@ orangee.facebook = (function () {
             queryString = url.substr(url.indexOf('#') + 1);
             obj = parseQueryString(queryString);
             tokenStore['fbtoken'] = obj['access_token'];
-            if (loginSuccessHandler) loginSuccessHandler();
+            if (loginCallback) loginCallback({status: 'connected', authResponse: {token: obj['access_token']}});
         } else if (url.indexOf("error=") > 0) {
             queryString = url.substring(url.indexOf('?') + 1, url.indexOf('#'));
             obj = parseQueryString(queryString);
-            if (loginErrorHandler) loginErrorHandler(obj);
+            if (loginCallback) loginCallback({status: 'not_authorized', error: obj.error});
         } else {
-            if (loginErrorHandler) loginErrorHandler();
+            if (loginCallback) loginCallback({status: 'not_authorized'});
         }
     }
 
     /**
-     * Application-level logout: we simply discard the token.
+     * Logout from Facebook, and remove the token.
+     * IMPORTANT: For the Facebook logout to work, the logoutRedirectURL must be on the domain specified in "Site URL" in your Facebook App Settings
+     *
      */
-    function logout() {
-        tokenStore['fbtoken'] = undefined;
+    function logout(callback) {
+        var logoutWindow,
+            token = tokenStore['fbtoken'];
+
+        /* Remove token. Will fail silently if does not exist */
+        tokenStore.removeItem('fbtoken');
+
+        if (token) {
+            logoutWindow = window.open(FB_LOGOUT_URL + '?access_token=' + token + '&next=' + logoutRedirectURL, '_blank', 'location=no');
+            if (runningInCordova) {
+                setTimeout(function() {
+                    logoutWindow.close();
+                }, 700);
+            }
+        }
+
+        if (callback) {
+            callback();
+        }
+
     }
 
     /**
@@ -232,7 +280,8 @@ orangee.facebook = (function () {
         logout: logout,
         revokePermissions: revokePermissions,
         api: api,
-        oauthCallback: oauthCallback
+        oauthCallback: oauthCallback,
+        getLoginStatus: getLoginStatus
     }
 
 }());
