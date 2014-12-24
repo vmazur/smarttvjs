d0a506865341a83f4c6ace8ef2b42219e45adecb
orangeejs <orangeejs@orangeejs.com>
2014-12-23 23:50:42 -0500

    spinner view

--- src/views.js
+++ src/views.js
@@ -28,7 +28,13 @@ OrangeeScrollerBehavior = Marionette.Behavior.extend({
     orangee.debug(this.view.getOption('scroll'));
     //orangee.debug(this.el.parentNode.parentNode);
     orangee.debug(this.el);
-    this.view.scroller = new orangee.scroller(this.el, this.view.getOption('scroll'));
+    //http://stackoverflow.com/questions/11924711/how-to-make-iscroll-and-lazy-load-jquery-plugins-work-together
+    this.view.scroller = new orangee.scroller(this.el,
+                                              _.extend({
+                                                onScrollEnd: function () {
+                                                  this.el.trigger('scroll');
+                                                },
+                                              }, this.view.getOption('scroll')));
     this.view.collection.selectModel(this.view.collection.at(this.view.collection.currentPosition));
     //orangee.debug(this.view);
   },
@@ -74,6 +80,34 @@ Orangee.CompositeView = Marionette.CompositeView.extend({
   },
   childViewOptions: function() {
     return {collectionView: this};
+  },
+});
+
+Orangee.SpinnerView = Marionette.ItemView.extend({
+  template: false,
+  opts: {
+    lines: 13, // The number of lines to draw
+    length: 20, // The length of each line
+    width: 10, // The line thickness
+    radius: 30, // The radius of the inner circle
+    corners: 1, // Corner roundness (0..1)
+    rotate: 0, // The rotation offset
+    direction: 1, // 1: clockwise, -1: counterclockwise
+    color: '#000', // #rgb or #rrggbb or array of colors
+    speed: 1, // Rounds per second
+    trail: 60, // Afterglow percentage
+    shadow: false, // Whether to render a shadow
+    hwaccel: false, // Whether to use hardware acceleration
+    className: 'spinner', // The CSS class to assign to the spinner
+    zIndex: 2e9, // The z-index (defaults to 2000000000)
+    top: '50%', // Top position relative to parent
+    left: '50%' // Left position relative to parent
+  },
+  onShow: function() {
+    this.spinner = new orangee.spinner(this.getOption('opts')).spin(this.el);
+  },
+  onDestroy: function() {
+    this.spinner.stop();
   },
 });
 
@@ -161,7 +195,7 @@ Orangee.ScrollView = Orangee.CompositeView.extend({
   },
   childViewContainer: "ul",
   scroll: {
-    click: true,
+    //click: true,
     mouseWheel: true,
     //keyBindings: true,
   },
