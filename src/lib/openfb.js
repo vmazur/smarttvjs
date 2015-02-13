8387f57f8f3447823fd76f57aff1a1bef837531e
orangeejs <orangeejs@orangeejs.com>
2015-02-13 12:07:35 -0500

    remove console.log

--- src/lib/openfb.js
+++ src/lib/openfb.js
@@ -35,8 +35,8 @@ var openFB = (function () {
         // Used in the exit event handler to identify if the login has already been processed elsewhere (in the oauthCallback function)
         loginProcessed;
 
-    console.log(oauthRedirectURL);
-    console.log(logoutRedirectURL);
+    orangee.debug(oauthRedirectURL);
+    orangee.debug(logoutRedirectURL);
 
     document.addEventListener("deviceready", function () {
         runningInCordova = true;
