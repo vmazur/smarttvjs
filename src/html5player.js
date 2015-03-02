546d8a1456def5e408d7c45acc52984d378d194c
orangeejs <orangeejs@orangeejs.com>
2015-03-01 23:26:17 -0500

    disable videojs onlg, it will crash lg's os

--- src/html5player.js
+++ src/html5player.js
@@ -3,6 +3,7 @@ orangee.html5player = function _OrangeeJSHTML5Player() {
   this.player = null;
   this.inactivityTimeout = null;
   this.support_translate = true;
+  this.native_controls = (orangee.PLATFORM == 'lg');//lg's os will crash with videojs if video is paused
 };
 
 orangee.html5player.prototype.play = function() {
@@ -66,71 +67,75 @@ orangee.html5player.prototype.load = function(url, startSeconds, divid, options)
       width:  options['width']  || "100%",
       height: options['height'] || "100%",
     };
-    var self = this;
-    videojs(divid, vjsopt, function(){
-      self.player = this;
-      orangee.debug("orangee.html5player#ready");
-      self._load(url, startSeconds, options);
-    });
-    //this._load(url, startSeconds, options);
+    if (this.native_controls) {
+      this._load(url, startSeconds, options);
+    } else {
+      var self = this;
+      videojs(divid, vjsopt, function(){
+        self.player = this;
+        orangee.debug("orangee.html5player#ready");
+        self._load(url, startSeconds, options);
+      });
+    }
   } else {
     this._load(url, startSeconds, options);
   }
 };
 
 orangee.html5player.prototype._load = function(url, startSeconds, options) {
-  /*
-  //this.video.width = options['width'] || '100%';
-  if (options['onplaying']) {
-    this.video.addEventListener("playing", options['onplaying']);
-  }
-  if (options['onpause']) {
-    this.video.addEventListener("pause", options['onpause']);
-  }
-  if (options['onend']) {
-    this.video.addEventListener("ended", options['onend']);
-  }
-  if (options['onerror']) {
-    this.video.addEventListener("error", options['onerror'], true);
-  }
-  if ((options['autoplay'] || 0) == 1) {
-    this.video.autoplay = true;
-  }
+  if (this.native_controls) {
+    //this.video.width = options['width'] || '100%';
+    if (options['onplaying']) {
+      this.video.addEventListener("playing", options['onplaying']);
+    }
+    if (options['onpause']) {
+      this.video.addEventListener("pause", options['onpause']);
+    }
+    if (options['onend']) {
+      this.video.addEventListener("ended", options['onend']);
+    }
+    if (options['onerror']) {
+      this.video.addEventListener("error", options['onerror'], true);
+    }
+    if ((options['autoplay'] || 0) == 1) {
+      this.video.autoplay = true;
+    }
 
-  this.video.controls = true;
-  orangee.debug("orangee.html5player.prototype._load " + url);
-  this.video.load();
-  if (startSeconds > 0) {
-    var self = this;
-    this.video.addEventListener("canplay",function() { 
-      orangee.debug("orangee.html5player#canplay");
-      self.video.currentTime = startSeconds;
-    });
-  }*/
-
-  if (options['onplaying']) {
-    this.player.on("playing", options['onplaying']);
-  }
-  if (options['onpause']) {
-    this.player.on("pause", options['onpause']);
-  }
-  if (options['onend']) {
-    this.player.on("ended", options['onend']);
-  }
-  if (options['onerror']) {
-    this.player.on("error", options['onerror'], true);
-  }
-  if (options['autoplay']) {
-    this.player.autoplay(true);
-  }
-  this.player.controls(true);
-  this.player.load();
-  if (startSeconds > 0) {
-    var self = this;
-    this.player.one("canplay",function() {
-      orangee.debug("orangee.html5player#canplay");
-      self.player.currentTime(startSeconds);
-    });
+    this.video.controls = true;
+    orangee.debug("orangee.html5player.prototype._load " + url);
+    this.video.load();
+    if (startSeconds > 0) {
+      var self = this;
+      this.video.addEventListener("canplay",function() { 
+        orangee.debug("orangee.html5player#canplay");
+        self.video.currentTime = startSeconds;
+      });
+    }
+  } else {
+    if (options['onplaying']) {
+      this.player.on("playing", options['onplaying']);
+    }
+    if (options['onpause']) {
+      this.player.on("pause", options['onpause']);
+    }
+    if (options['onend']) {
+      this.player.on("ended", options['onend']);
+    }
+    if (options['onerror']) {
+      this.player.on("error", options['onerror'], true);
+    }
+    if (options['autoplay']) {
+      this.player.autoplay(true);
+    }
+    this.player.controls(true);
+    this.player.load();
+    if (startSeconds > 0) {
+      var self = this;
+      this.player.one("canplay",function() {
+        orangee.debug("orangee.html5player#canplay");
+        self.player.currentTime(startSeconds);
+      });
+    }
   }
 };
 
